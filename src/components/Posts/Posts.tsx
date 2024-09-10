bfc11cc0d621d5632e99a6a9cd761cafff4cd346
Rasel Shikdar <103440692+raselshikdar@users.noreply.github.com>
2024-09-10 14:59:17 +0600

    Update Posts.tsx

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -175,7 +175,7 @@ const Posts: React.FC<Props> = ({ posts }) => {
               type="checkbox"
               name="detailView"
               id="detailView"
-              checked={detailView}
+              checked={displayDetail}
               onChange={() => setMode(mode === 'detail' ? 'normal' : 'detail')}
             />
             <label htmlFor="detailView">detail</label>
