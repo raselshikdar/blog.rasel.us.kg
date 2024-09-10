16953966d510a3ac206b9d31ee05cba21b8892a8
Rasel Shikdar <103440692+raselshikdar@users.noreply.github.com>
2024-09-10 14:44:33 +0600

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -12,7 +12,7 @@ const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles['footer__copyright']}>
-        © 2024 / {name.toUpperCase()}
+        © 2024 <a href="https://rasel.us.kg">{name.toUpperCase()}</a><br />All rights are reserved.
       </div>
     </footer>
   )
